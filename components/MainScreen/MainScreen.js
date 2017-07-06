791907a3a751a770810861b54f9691ed3d284556
Mikael Midtgaard <mikaelmidt@gmail.com>
2017-07-06 22:13:10 +0200

    Initial styling of MainScreen

--- components/MainScreen/MainScreen.js
+++ components/MainScreen/MainScreen.js
@@ -1,7 +1,38 @@
 import React from 'react';
-import { View, Button } from 'react-native';
+import { StyleSheet, View, Text, Button, Image } from 'react-native';
 import { connect } from 'react-redux';
+import fontSizes from '../../constants/fontSizes';
 
+const styles = StyleSheet.create({
+  background: {
+    display: 'flex',
+    flex: 1,
+    width: null,
+    height: null,
+    resizeMode: 'cover',
+    alignItems: 'center',
+    justifyContent: 'space-around'
+  },
+  headLineContainer: {
+    flex: 2,
+  },
+  headline: {
+    fontSize: fontSizes.huge,
+    color: 'white',
+    fontWeight: '900',
+  },
+  startButtonContainer: {
+    flex: 4,
+  },
+  startButton: {
+
+  },
+  center: {
+    justifyContent: 'center',
+    alignItems: 'center',
+  }
+});
+//
 class MainScreen extends React.Component {
   constructor(props) {
     super(props);
@@ -14,11 +45,35 @@ class MainScreen extends React.Component {
   }
 
   render() {
-    return <View>
-      <Button title='PLAY' onPress={this.onPlayPressed} />
-    </View>;
+    return <Image
+      source={require('../../assets/img/background_boxes.png')}
+      style={styles.background}
+    >
+      <View
+        style={[styles.headLineContainer, styles.center]}
+      >
+        <Text
+          style={styles.headline}
+        >
+          Title of the game
+        </Text>
+      </View>
+      <View
+        style={[styles.startButtonContainer, styles.center]}
+      >
+        <Button
+          title='PLAY'
+          style={styles.startButton}
+          onPress={this.onPlayPressed}
+        />
+      </View>
+    </Image>;
   }
 }
+
+MainScreen.navigationOptions = {
+  header: null,
+};
 
 export default connect(state => ({
     // gameState: state.gameState,
--- constants/fontSizes.js
+++ constants/fontSizes.js
@@ -1,5 +1,6 @@
 export default {
   small: 10,
   medium: 15,
-  large: 20
+  large: 20,
+  huge: 40,
 };
